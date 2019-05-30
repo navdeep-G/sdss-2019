882a0c8fb72a99371c73520cc39f8079fd3a59be
navdeep-G <mr.navdeepgill@gmail.com>
2019-05-30 11:37:09 -0700

    More data prep

--- r/rsparkling_mli.R
+++ r/rsparkling_mli.R
@@ -47,8 +47,7 @@ sc <- spark_connect(master = "local", version = "2.4.0")
 h2o_context(sc)
 
 # We can also view the H2O Flow web UI:
-h2o_flow(sc)
-
+# h2o_flow(sc)
 
 # H2O with Spark DataFrames
 
@@ -56,19 +55,39 @@ h2o_flow(sc)
 library(dplyr)
 train_tbl <- spark_read_csv(sc, "../data/creditcard-train.csv")
 test_tbl <- spark_read_csv(sc, "../data/creditcard-test.csv")
-train_tbl
-test_tbl
 
 # Convert the Spark DataFrame's into an H2OFrame's
 train_hex <- as_h2o_frame(sc, train_tbl)
 test_hex <- as_h2o_frame(sc, test_tbl)
-train_hex
-test_hex
 
-# Split the H2O Frame into train & test sets
-#splits <- h2o.splitFrame(train_hex, ratios = 0.7, seed = 1)
-#nrow(splits[[1]])  # nrows in train
-#nrow(splits[[2]])  # nrows in test
+# Modify columns for train
+train_hex$SEX = as.factor(train_hex$SEX)
+train_hex$MARRIAGE = as.factor(train_hex$MARRIAGE)
+train_hex$EDUCATION = as.factor(train_hex$EDUCATION)
+train_hex$PAY_0 = as.factor(train_hex$PAY_0)
+train_hex$PAY_2 = as.factor(train_hex$PAY_2)
+train_hex$PAY_3 = as.factor(train_hex$PAY_3)
+train_hex$PAY_4 = as.factor(train_hex$PAY_4)
+train_hex$PAY_5 = as.factor(train_hex$PAY_5)
+train_hex$PAY_6 = as.factor(train_hex$PAY_6)
+train_hex$DEFAULT_PAYMENT_NEXT_MONTH = as.factor(train_hex$DEFAULT_PAYMENT_NEXT_MONTH)
+
+# Modify columns for test
+test_hex$SEX = as.factor(test_hex$SEX)
+test_hex$MARRIAGE = as.factor(test_hex$MARRIAGE)
+test_hex$EDUCATION = as.factor(test_hex$EDUCATION)
+test_hex$DEFAULT_PAYMENT_NEXT_MONTH = as.factor(test_hex$DEFAULT_PAYMENT_NEXT_MONTH)
+test_hex$PAY_0 = as.factor(test_hex$PAY_0)
+test_hex$PAY_2 = as.factor(test_hex$PAY_2)
+test_hex$PAY_3 = as.factor(test_hex$PAY_3)
+test_hex$PAY_4 = as.factor(test_hex$PAY_4)
+test_hex$PAY_5 = as.factor(test_hex$PAY_5)
+test_hex$PAY_6 = as.factor(test_hex$PAY_6)
+
+# Split the H2O Frame train and validation
+splits <- h2o.splitFrame(train_hex, ratios = 0.7, seed = 12345)
+nrow(splits[[1]])  # nrows in train
+nrow(splits[[2]])  # nrows in validation
 
 # Train an H2O Gradient Boosting Machine (GBM)
 # And perform 3-fold cross-validation via `nfolds`
@@ -76,13 +95,20 @@ y <- "DEFAULT_PAYMENT_NEXT_MONTH"
 x <- setdiff(names(train_hex), y)
 fit <- h2o.gbm(x = x,
                y = y,
-               training_frame = train_hex,
-               nfolds = 3,
-               min_rows = 1,
-               seed = 1)
+               ntrees = 150, # maximum 150 trees in GBM
+               max_depth = 4, # trees can have maximum depth of 4
+               sample_rate = 0.9, # use 90% of rows in each iteration (tree)
+               col_sample_rate = 0.9, # use 90% of variables in each iteration (tree)
+               balance_classes = TRUE, # sample to balance 0/1 distribution of target
+               stopping_rounds = 5,  # stop if validation error does not decrease for 5 iterations (trees)
+               score_tree_interval = 1,  # for reproducibility, set higher for bigger data
+               training_frame = splits[[1]], # training frame
+               validation_frame = splits[[2]], # validation frame
+               seed = 12345 # Seed for reproducibility
+               ) 
 
-# Evaluate 3-fold cross-validated model performance:
-h2o.performance(fit, xval = TRUE)
+# Evaluate model performance on validation:
+h2o.performance(fit, valid = TRUE)
 
 # As a comparison, we can evaluate performance on a test set
 h2o.performance(fit, newdata = test_hex)
